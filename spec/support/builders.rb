f80a89313326010a938f7f226f71591a783bee2a
Kristján Pétursson <kristjan@gmail.com>
2011-02-28 20:40:42 -0800

    Longer slugs in specs
    There were slug collisions; need some more entropy.

--- spec/support/builders.rb
+++ spec/support/builders.rb
@@ -6,7 +6,7 @@ Fixjour do
   define_builder(Site) do |klass, overrides|
     klass.new(
       :name => Faker::Lorem.sentence(3),
-      :slug => Faker::Lorem.word.downcase
+      :slug => Faker::Lorem.words(2).join.downcase
     )
   end
 
