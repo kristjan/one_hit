386a2f62068a7d1c647c2955ae074d6bc50178ba
Kristján Pétursson <kristjan@gmail.com>
2011-03-02 01:23:21 -0800

    Compass; Sites#new template

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'http://rubygems.org'
 
 gem 'rails', '3.0.4'
+gem 'compass'
 gem 'haml'
 
 group :development, :test do
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ OneHit::Application.routes.draw do
   resources :sites, :path => '/' do
     resources :quips
   end
+
+  root :to => "sites#index"
 end
