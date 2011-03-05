5e31db315467eced74724198f7ffbf53aba79dfb
Kristján Pétursson <kristjan@gmail.com>
2011-03-05 15:41:35 -0800

    Clean form errors; cleanup CSS

--- app/models/site.rb
+++ app/models/site.rb
@@ -5,7 +5,9 @@ class Site < ActiveRecord::Base
   accepts_nested_attributes_for :quips, :allow_destroy => true
 
   validates_length_of :name, :minimum => 1
-  validates_format_of :slug, :with => /^[-_a-z1-9]+$/i
+  validates_length_of :slug, :minimum => 1
+  validates_format_of :slug, :with => /^[-_a-z1-9]+$/i, :allow_blank => true,
+    :message => "can include letters, numbers, dashes and underscores"
   validates_uniqueness_of :slug
 
   def random_quip
