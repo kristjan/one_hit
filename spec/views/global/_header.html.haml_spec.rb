f65822235f80fa0ad4727c4cc74ea0566f4ecf80
Kristján Pétursson <kristjan@gmail.com>
2011-03-07 21:37:17 -0800

    Persisten New Hit link; converge on Hit not Site

--- spec/views/global/_header.html.haml_spec.rb
+++ spec/views/global/_header.html.haml_spec.rb
@@ -3,12 +3,17 @@ require 'spec_helper'
 describe "global/_header.html.haml" do
   it "renders a link to Home" do
     render
-    assert_select "a[href=#{root_path}]"
+    assert_select "a[href=?]", root_path
+  end
+
+  it "renders a link to make a new site" do
+    render
+    assert_select "a[href=?]", new_site_path
   end
 
   it "renders a link to a random site" do
     render
-    assert_select "a[href=#{random_sites_path}]"
+    assert_select "a[href=?]", random_sites_path
   end
 
   it "renders a title when there is one" do
