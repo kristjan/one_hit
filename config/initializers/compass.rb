9303911d9ae7ac9f64da03b95cb3112273f09982
Kristján Pétursson <kristjan@gmail.com>
2011-04-02 14:28:36 -0700

    Don't update Compass files in production
    This may be the reason for first-request failure in Heroku, as it's a read-only
    filesystem

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,3 +1,8 @@
 require 'compass'
 require 'compass/app_integration/rails'
 Compass::AppIntegration::Rails.initialize!
+
+if Rails.env.production?
+  Compass.configuration.sass_options ||= {}
+  Compass.configuration.sass_options[:never_update] = true
+end
