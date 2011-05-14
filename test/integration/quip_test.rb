41a9e2e1150a871a96f880361c84fa53f50d12cd
Kristján Pétursson <kristjan@gmail.com>
2011-05-14 16:44:28 -0700

    Delete unused files

--- test/integration/quip_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class QuipTest < ActionDispatch::IntegrationTest
-  fixtures :all
-
-  test "the truth" do
-    assert true
-  end
-end
--- test/integration/site_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class SiteTest < ActionDispatch::IntegrationTest
-  fixtures :all
-
-  test "the truth" do
-    assert true
-  end
-end
