69f984c4644e5687181bc5e99d23f700d2aa6b5f
Kristján Pétursson <kristjan@gmail.com>
2011-05-15 19:33:29 -0700

    Render random sites on the landing page

--- spec/views/sites/index.html.haml_spec.rb
+++ spec/views/sites/index.html.haml_spec.rb
@@ -4,6 +4,8 @@ describe "sites/index.html.haml" do
   include ContentForTestHelper
 
   before :each do
+    @sites = assign(:sites, [new_site, new_site, new_site])
+    @sites.each {|site| site.stub(:random_quip).and_return(new_quip)}
     render
   end
 
@@ -17,5 +19,17 @@ describe "sites/index.html.haml" do
 
   it "renders a link to the thanks page" do
     assert_select "a[href=?]", thanks_path
+  end
+
+  it "renders a set of sites" do
+    render
+    assert_select ".examples" do
+      @sites.each do |site|
+        assert_select ".site" do
+          assert_select "h1", :text => site.name
+          assert_select "p", :text => site.random_quip.text
+        end
+      end
+    end
   end
 end
