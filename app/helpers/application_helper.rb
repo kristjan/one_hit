2999463afa77b908bb311b9a2ee971afa1d8812e
Kristján Pétursson <kristjan@gmail.com>
2011-05-28 18:47:41 -0700

    Fontify landing page header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,21 @@
 require 'set'
 
 module ApplicationHelper
+  GOOGLE_FONT_BASE = "http://fonts.googleapis.com/css"
+
   def add_stylesheet(sheet)
     (@stylesheets ||= Set.new) << sheet
   end
 
   def stylesheets
     @stylesheets ||= Set.new
+  end
+
+  def google_font_link_tag(font)
+    font = font.gsub(/\s+/, '+')
+    tag(:link,
+        :href => "#{GOOGLE_FONT_BASE}?family=#{font}:regular,b,i,bi",
+        :rel => 'stylesheet', :type => 'text/css')
   end
 
   def title(title)
