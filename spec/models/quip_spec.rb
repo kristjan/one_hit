320baa4677c0b6188a5de4f3711c43220b32b6b7
Kristján Pétursson <kristjan@gmail.com>
2011-06-25 18:20:15 -0700

    Allow nesting of Quips during Site creation
    There's a circular dependency when a nested child object tries to verify its
    parent during creation. The parent has been validated, but not yet saved, so it
    doesn't have an ID yet. The child will complain that its parent can't be blank.
    
    Work around this by adding a :nested attribute to Quips that gets set during a
    nested creation. In this case, it doesn't try to validate the Site.

--- spec/models/quip_spec.rb
+++ spec/models/quip_spec.rb
@@ -13,4 +13,11 @@ describe Quip do
   it "belongs to a Site" do
     new_quip.site.should be_a(Site)
   end
+
+  context "when nested" do
+    it "doesn't validate the site" do
+      quip = new_quip(:site => nil, :nested => true)
+      quip.should be_valid
+    end
+  end
 end
